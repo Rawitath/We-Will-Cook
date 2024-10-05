b81f563c7a2773146c529326f722ff78029552bb
Rawitath <67070149@kmitl.ac.th>
2024-10-05 19:34:18 +0700

    Create React Pages with Routing
    Pages Created with .jsx and .css
    
    react-router-dom is installed so don't forget to npm install in frontend folder

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+export default function Home(){
+    return(
+        <>
+        <div className='headdiv'>
+            <h1 className='headtext'>This is Home Page</h1>
+        </div>
+        <p>We Will Cook!</p>
+        </>
+    )
+}
