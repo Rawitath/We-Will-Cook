a92c3fa6462bd0a3f42826862c240e1c5d56f2aa
Rawitath <67070149@kmitl.ac.th>
2024-10-10 04:32:03 +0700

    Create Api and Endpoint for authentication

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib import messages
+
+from rest_framework import generics
+from .models import *
+from .serializer import *
 # Create your views here.
+class WWCUserView(generics.ListCreateAPIView):
+    queryset = WWCUser.objects.all()
+    serializer_class = WWCUserSerializer
+
 def signup(request):
     # if request.method == "POST":
     #     username = request.POST['username']
