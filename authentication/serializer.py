d02778a1d3e8326f25e573ce6bd2ae3177bf3b3f
Rawitath <67070149@kmitl.ac.th>
2024-10-10 19:07:33 +0700

    Make password hash when create user

--- authentication/serializer.py
+++ authentication/serializer.py
@@ -5,3 +5,9 @@ class WWCUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = WWCUser
         fields = ['userid', 'username', 'email', 'password', 'profile_pic']
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
