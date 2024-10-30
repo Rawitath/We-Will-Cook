d942c4c89f9873b0fffeff61d9e8b08d2a2d5503
Rawitath <67070149@kmitl.ac.th>
2024-10-31 04:35:56 +0700

    Create base model for tastes and serializer header

--- cooking/models.py
+++ cooking/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class CondimentModel(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    # Tastes is kept as dict parsed to json
+    tastes = models.CharField(max_length=256)
+    def set_tastes(self, tastes_dict):
+        import json
+        return json.dumps(tastes_dict ,ensure_ascii=False).encode("utf-8")
+    def get_tastes(self):
+        import json
+        return json.loads(self.tastes, ensure_ascii=False)
+
+
+class TastePreferenceModel(models.Model):
+    userid = models.CharField(max_length=256, unique=True)
+    # Tastes is kept as dict parsed to json
+    tastes = models.CharField(max_length=256)
+    # WIP naja
