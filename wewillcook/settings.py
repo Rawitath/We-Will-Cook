556529cd17b20f241f151bb55c1eff318bb00bf6
Rawitath <67070149@kmitl.ac.th>
2024-11-06 03:04:15 +0700

    Increase lifetime of refresh token

--- wewillcook/settings.py
+++ wewillcook/settings.py
@@ -76,7 +76,7 @@ from datetime import timedelta
 #region JWT
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1095.7266), # Until We Graduate
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
